4bd49906dd7f815a2713e4df0f35c12ac340a558
FabioPrudencio <fabio.oliveira.prudencio@gmail.com>
2020-09-02 23:36:46 -0300

    Ajustes no endpoint /categorias/{id} e criação do tratamento de exceções

--- src/main/java/com/fabioprudencio/cursomc/services/CategoriaService.java
+++ src/main/java/com/fabioprudencio/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.fabioprudencio.cursomc.domain.Categoria;
 import com.fabioprudencio.cursomc.repositories.CategoriaRepository;
+import com.fabioprudencio.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,9 +15,15 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	/*(public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElse(null);
+	}*/
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
