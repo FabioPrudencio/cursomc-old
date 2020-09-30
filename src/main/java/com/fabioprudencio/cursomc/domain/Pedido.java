e05294f69a3ca65832daa3330c0fb2dea4b3914f
FabioPrudencio <fabio.oliveira.prudencio@gmail.com>
2020-09-29 23:43:07 -0300

    ItemPedido e ItemPedido PK parte 1

--- src/main/java/com/fabioprudencio/cursomc/domain/Pedido.java
+++ src/main/java/com/fabioprudencio/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.fabioprudencio.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -31,6 +33,8 @@ public class Pedido implements Serializable {
 	@ManyToOne
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEndereco;	
+	
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	public Pedido() {
 		
@@ -82,6 +86,14 @@ public class Pedido implements Serializable {
 
 	public void setEnderecoDeEndereco(Endereco enderecoDeEndereco) {
 		this.enderecoDeEndereco = enderecoDeEndereco;
+	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
